99a6a622c623817f44fce8f2a38f43864071ed7c
Emily Overend <eo17233@gmail.com>
2020-12-28 22:35:45 +0000

    Automatically backed up by Learn

--- lib/move.rb
+++ lib/move.rb
@@ -7,19 +7,13 @@ def display_board(board)
 end
 
 # code your input_to_index and move method here!
-def input_to_index('1'-'9')
-  puts "0 + 1"
-  puts "1 + 1"
-  puts "2 + 1"
-  puts "3 + 1"
-  puts "4 + 1"
-  puts "5 + 1"
-  puts "6 + 1"
-  puts "7 + 1"
-  puts "8 + 1"
+def input_to_index(user_input)
+  new_user_input = user_input.to_i
+  new_user_input -= 1
+  return new_user_input
 end
 
-board = [" ", " ", " ", " ", " ", " ", " ", " ", " "]
-def update_array_at_with(board, 0, "X")
-  array[index] = value
+def move(board, index, character = "X")
+  board[index] = character
+  return board
 end
